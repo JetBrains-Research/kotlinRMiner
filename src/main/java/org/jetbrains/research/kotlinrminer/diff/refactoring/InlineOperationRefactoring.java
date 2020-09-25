619fc943e1f348e4732485ba3d02fc2c39c73c83
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2020-09-25 14:45:39 +0300

    Add a test that checks the detection of Inline Method refactoring

--- src/main/java/org/jetbrains/research/kotlinrminer/diff/refactoring/InlineOperationRefactoring.java
+++ src/main/java/org/jetbrains/research/kotlinrminer/diff/refactoring/InlineOperationRefactoring.java
@@ -41,7 +41,7 @@ public class InlineOperationRefactoring implements Refactoring {
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(getName()).append("\t");
+        sb.append(getName()).append(" ");
         sb.append(inlinedOperation);
         if (getRefactoringType().equals(RefactoringType.INLINE_OPERATION)) {
             sb.append(" inlined to ");
