ece8a5de96551cd278f6a05427f3f1054e13e224
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2020-09-29 14:11:39 +0300

    Add tests (Extract Method, Rename Method, and parameter-related refactorings)

--- src/main/java/org/jetbrains/research/kotlinrminer/diff/refactoring/ExtractOperationRefactoring.java
+++ src/main/java/org/jetbrains/research/kotlinrminer/diff/refactoring/ExtractOperationRefactoring.java
@@ -61,7 +61,7 @@ public class ExtractOperationRefactoring implements Refactoring {
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(getName()).append("\t");
+        sb.append(getName()).append(" ");
         sb.append(extractedOperation);
         sb.append(" extracted from ");
         sb.append(sourceOperationBeforeExtraction);
