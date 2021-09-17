f59b78fe088b696eb441ac6a05175e4f0f3156b9
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2021-09-17 19:59:36 +0300

    do not build fat jar for API

--- KotlinRMiner-CLI/build.gradle.kts
+++ KotlinRMiner-CLI/build.gradle.kts
@@ -1,3 +1,13 @@
 dependencies {
     implementation(project(":KotlinRMiner"))
 }
+
+tasks.jar {
+    manifest {
+        attributes["Implementation-Version"] = archiveVersion
+        attributes["Main-Class"] = "org.jetbrains.research.kotlinrminer.KotlinRMiner"
+    }
+
+    exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA", "misc/**")
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
